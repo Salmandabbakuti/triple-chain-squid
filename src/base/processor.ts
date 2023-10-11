76e913322bf3a97345ea61c811be0dfbd3c0ce2d
Salman Dabbakuti <dabbakutisalman@gmail.com>
2023-10-11 19:24:09 +0530

    updated: using public archive for indexing, subscriptions

--- src/base/processor.ts
+++ src/base/processor.ts
@@ -1,39 +1,40 @@
-import {lookupArchive} from '@subsquid/archive-registry'
+import { lookupArchive } from "@subsquid/archive-registry";
 import {
-    BlockHeader,
-    DataHandlerContext,
-    EvmBatchProcessor,
-    EvmBatchProcessorFields,
-    Log as _Log,
-    Transaction as _Transaction,
-} from '@subsquid/evm-processor'
-import {Store} from '@subsquid/typeorm-store'
-import * as erc20abi from '../abi/erc20'
+  BlockHeader,
+  DataHandlerContext,
+  EvmBatchProcessor,
+  EvmBatchProcessorFields,
+  Log as _Log,
+  Transaction as _Transaction
+} from "@subsquid/evm-processor";
+import { Store } from "@subsquid/typeorm-store";
+import * as erc20abi from "../abi/erc20";
 
-export const BASE_USDBC_ADDRESS = '0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA'.toLowerCase()
+export const BASE_USDC_ADDRESS =
+  "0x951F55f0492a638Ff1d670aCd4B27b51CA0Ad4d1".toLowerCase();
 
 export const processor = new EvmBatchProcessor()
-    .setDataSource({
-        archive: 'http://localhost:8000/network/base-mainnet',
-        // Disabled for quests to avoid DDoSing Ankr :)
-        //chain: 'https://rpc.ankr.com/base',
-    })
-    .setFinalityConfirmation(75)
-    .setFields({
-        log: {
-            transactionHash: true
-        }
-    })
-    .setBlockRange({
-        from: 3_800_000,
-    })
-    .addLog({
-        address: [BASE_USDBC_ADDRESS],
-        topic0: [erc20abi.events.Transfer.topic]
-    })
+  .setDataSource({
+    archive: lookupArchive("base-goerli")
+    // Disabled for quests to avoid DDoSing Ankr :)
+    // chain: "https://rpc.ankr.com/base"
+  })
+  .setFinalityConfirmation(75)
+  .setFields({
+    log: {
+      transactionHash: true
+    }
+  })
+  .setBlockRange({
+    from: 5_918_310
+  })
+  .addLog({
+    address: [BASE_USDC_ADDRESS],
+    topic0: [erc20abi.events.Transfer.topic]
+  });
 
-export type Fields = EvmBatchProcessorFields<typeof processor>
-export type Context = DataHandlerContext<Store, Fields>
-export type Block = BlockHeader<Fields>
-export type Log = _Log<Fields>
-export type Transaction = _Transaction<Fields>
+export type Fields = EvmBatchProcessorFields<typeof processor>;
+export type Context = DataHandlerContext<Store, Fields>;
+export type Block = BlockHeader<Fields>;
+export type Log = _Log<Fields>;
+export type Transaction = _Transaction<Fields>;
